56c96fea3f818af1b75861d2b386bf678c46dfbb
Ayman Ahmed Ebrahim <ayman.ahmed.ebrahim95@gmail.com>
2017-02-25 21:08:50 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.robot.ex001;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void bu_con(View view){
+        Intent intent1=new Intent(this,Main2Activity.class);
+
+        startActivity(intent1);
+    }
+
+
+
+}
